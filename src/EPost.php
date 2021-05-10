982c0a117b3aaf0e04c1d97c9cfff9203844b646
Raymond Chong <mathsgod@yahoo.com>
2021-05-10 17:47:35 +0800

    get SMS Report

--- src/EPost.php
+++ src/EPost.php
@@ -32,4 +32,29 @@ class EPost
 
         return $resp["data"]["sendSMS"][0];
     }
+
+    public function getSMSReport(string $start, string $end)
+    {
+        $resp = $this->client->query([
+            "SMS" => [
+                "__args" => [
+                    "filter" => [
+                        "created_time" => [
+                            "between" => [$start . " 00:00:00", $end . " 23:59:59"]
+                        ]
+                    ]
+                ],
+                "list" => [
+                    "sms_id",
+                    "phone",
+                    "content",
+                    "send_time",
+                    "no_of_msg",
+                    "receive_time"
+                ]
+            ]
+        ]);
+
+        return $resp["data"]["SMS"]["list"];
+    }
 }
